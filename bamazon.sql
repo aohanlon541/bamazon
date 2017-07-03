8b977bf8a05dbe38fe87a3079a271167daf26f28
aohanlon541 <allison.t.ohanlon@gmail.com>
2017-07-03 18:41:10 -0500

    beginning step 3

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,10 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
+CREATE TABLE departments ( 
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(45) NULL,
+  over_head_costs INT(7) NULL,
+  
+  PRIMARY KEY (department_id)
+);
